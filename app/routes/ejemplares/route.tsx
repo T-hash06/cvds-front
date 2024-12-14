931e03a7e285db03acdf099d9fd1bc82dbe83875
Juan Sebastian Velasquez <sebastianvelasquez2807@gmail.com>
2024-12-13 22:40:05 -0500

    feat: Implementando vista general

--- app/routes/ejemplares/route.tsx
+++ app/routes/ejemplares/route.tsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import MainLayout from '../../components/layouts/MainLayout';
 import {
 	Button,
 	Checkbox,
@@ -246,6 +247,7 @@ const EjemplaresPage = () => {
 	);
 
 	return (
+		<MainLayout>
 		<div className='p-6 bg-blue-50 min-h-screen'>
 			<div className='flex items-center justify-between mb-6'>
 				<Button
@@ -255,7 +257,7 @@ const EjemplaresPage = () => {
 					← Volver a Libros
 				</Button>
 				<h1 className='text-3xl font-bold text-blue-700'>
-					Gestión de Ejemplares para {nombreLibro}
+					Gestión de Ejemplares
 				</h1>
 				<Button
 					className='button-primary'
@@ -282,7 +284,9 @@ const EjemplaresPage = () => {
 					X
 				</Button>
 			</div>
-
+			<div className='p-6 bg-white shadow-md rounded-lg mb-6'>
+				<h1 className='text-2xl font-semibold text-blue-700'>{nombreLibro}</h1>
+			</div>
 			<Table aria-label='Lista de ejemplares'>
 				<TableHeader>
 					<TableColumn className='table-header'>Estado</TableColumn>
@@ -440,6 +444,7 @@ const EjemplaresPage = () => {
 				</Modal>
 			)}
 		</div>
+		</MainLayout>
 	);
 };
 
