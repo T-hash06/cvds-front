350f1c9fa318ff5816e86d590e023927af447d2e
Diego Cardenas <diegosp778@gmail.com>
2024-12-14 01:27:57 -0500

    feat: impl login and intance axios

--- app/components/layouts/MainLayout.tsx
+++ app/components/layouts/MainLayout.tsx
@@ -2,12 +2,28 @@ import { Outlet } from '@remix-run/react';
 import type { FC, ReactNode } from 'react';
 import Navbar from '../navbar/navbarUser';
 import Sidebar from '../sidebar/Sidebar';
+import cookies from 'js-cookie';
+import { useNavigate } from '@remix-run/react';
 
+const getToken = () => {
+	const token = cookies.get('$$id');
+    if (!token) {
+		return null;
+	} else {
+		return token;
+	}
+};
 interface MainLayoutProps {
 	children: ReactNode;
 }
 
 const MainLayout: FC<MainLayoutProps> = ({ children }) => {
+	const navigate = useNavigate();
+	if (!getToken()) {
+		navigate('/auth/login');
+	} else {
+		navigate('/general');
+	}
 	return (
 		<div className={'main-layout'}>
 			<Navbar />
